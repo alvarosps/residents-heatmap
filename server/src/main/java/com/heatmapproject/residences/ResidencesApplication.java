a7af70206c0eaa3f78d59a7f7b92c66654d68103
Alvaro Silva <alvaro123sps@gmail.com>
2021-03-03 13:44:41 -0300

    Created application, Client is with React(create-react-app) and Server is with Java(Spring Boot Initializr)

---
+++ server/src/main/java/com/heatmapproject/residences/ResidencesApplication.java
@@ -0,0 +1,13 @@
+package com.heatmapproject.residences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidencesApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/heatmapproject/residences/ResidencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatmapproject.residences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
